a8fb8c84fdd81387995dde4d7577f8aed3b576b4
Dodowingster <30728214+Dodowingster@users.noreply.github.com>
2017-08-05 10:06:43 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,149 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+    int stnum, courses, arr[100];
+    float arr2[6], arr3[20];
+    int counter, counter2, id, sum=0, sum2=0;
+    float gpa, gp[20], gpch;
+    char grd;
+
+    //Receiving the number of students to be calculated
+     printf("Please enter the number of student: ");
+     scanf("%d", &stnum);
+
+     //Guaranteeing the maximum(above 20) and minimum(below 0) number of students
+     if(stnum>20 || stnum<0)
+                {
+                    printf("Invalid user input. Please enter from 1-20. Thank you");
+                }else
+                {
+                    printf("How many courses does each student(s) register?: ");
+                    scanf("%d", &courses);
+
+                //Guaranteeing the minimum(below 0) number of courses taken
+                if(courses<=0)
+                {
+                    printf("Invalid user input. Please enter a valid number.");
+                }else
+                {
+
+                //Asking for the student's respective details
+                for(counter2=1; counter2<=stnum; counter2++)
+                    {
+                        printf("\nStudent %d:\n\n", counter2);
+
+
+                        printf("Please enter the student ID: ");
+                        scanf("%d", &id);
+
+                        for(counter=0; counter<courses; counter++)
+                        {
+
+                        printf("Please enter credit hour for course %d: ", counter+1);
+                        scanf("%f", &arr2[counter]);
+                        printf("Please enter total mark for course %d: ", counter+1);
+                        scanf("%d", &arr[counter]);
+
+
+                        //arr[counter] = Total Marks of the course //arr2[counter] = Credit Hour
+                        //Converting the Total Mark(arr[x]) into Grades
+                        if(arr[counter]>=85&&arr[counter]<=100)
+                        {
+                            grd = 'A';
+                            gp[counter] = 4.00;
+                        }
+                        else if(arr[counter]>80&&arr[counter]<84)
+                        {
+                            grd = 'A-';
+                            gp[counter] = 3.70;
+                        }
+                        else if(arr[counter]>=75&&arr[counter]<=79)
+                        {
+                            grd = 'B+';
+                            gp[counter] = 3.30;
+                        }
+                        else if(arr[counter]>=70&&arr[counter]<=74)
+                        {
+                            grd = 'B';
+                            gp[counter] = 3.00;
+                        }
+                        else if(arr[counter]>=65&&arr[counter]<=69)
+                        {
+                            grd = 'B-';
+                            gp[counter] = 2.70;
+                        }
+                        else if(arr[counter]>=60&&arr[counter]<=64)
+                        {
+                            grd = 'C+';
+                            gp[counter] = 2.30;
+                        }
+                        else if(arr[counter]>=55&&arr[counter]<=59)
+                        {
+                            grd = 'C';
+                            gp[counter] = 2.00;
+                        }
+                        else if(arr[counter]>=50&&arr[counter]<=54)
+                        {
+                            grd = 'C-';
+                            gp[counter] = 1.70;
+                        }
+                        else if(arr[counter]>=45&&arr[counter]<=49)
+                        {
+                            grd = 'D+';
+                            gp[counter] = 1.30;
+                        }
+                        else if(arr[counter]>=40&&arr[counter]<=44)
+                        {
+                            grd = 'D';
+                            gp[counter] = 1.00;
+                        }
+                        else if(arr[counter]<40)
+                        {
+                            grd = 'E';
+                            gp[counter] = 0.00;
+                        }
+
+                        //Setting every Grade Point * Credit Hour for EVERY course
+                        arr3[counter] = arr2[counter] * gp[counter];
+
+                        //Setting the total Credit Hours for ONE STUDENT
+                        sum = sum + arr2[counter];
+
+                        //Setting the total of Grade Point * Credit Hour for EVERY student
+                        sum2 = sum2 + arr3[counter];
+
+                    }
+
+                }
+
+            }
+
+            printf("\n\n\n============\n");
+            printf("RESULT INFO:\n");
+            printf("============\n\n");
+
+
+            printf("Student Id\tCourse\tCredit Hour\tMark\tGrade\tGrade Point\tGP*CH\n");
+
+
+                    for(counter=1; counter<=stnum; counter++)
+                    {
+
+                    printf("\t %d", counter);
+
+                            for(counter2=1; counter2<=courses; counter2++)
+                            {
+                                 printf("\t %d\t %.2f\t\t %d\t %c\t %.2f\t\t %.2f", counter2, arr2[counter], arr[counter], grd, gp[counter], arr3[counter]);
+                                 printf("\n\t");
+                            }
+
+
+            }
+
+
+     }
+}
